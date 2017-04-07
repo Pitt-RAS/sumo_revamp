fd6082ea8f1561242ff65f2810365e14ed1454d5
asaba96 <asaba96@pitt.edu>
2017-04-06 20:16:47 -0400

    Fixed the prox sensor test sketch to work with both ramps

--- test_sketches/Prox_test/Prox_test.ino
+++ test_sketches/Prox_test/Prox_test.ino
@@ -3,34 +3,33 @@
 #include "config.h"
 #include "ProxSense.h"
 
-int proximity_sensors_front[5]        = {F_PROX1_PIN, F_PROX2_PIN, F_PROX3_PIN, F_PROX4_PIN, F_PROX5_PIN};
-int proximity_sensors_front_weight[5] = {-90,         -45,         0,           45,          90};
+int front_pin_array[5] = {F_PROX1_PIN, F_PROX2_PIN, F_PROX3_PIN, F_PROX4_PIN, F_PROX5_PIN};
+int rear_pin_array[5]  = {R_PROX1_PIN, R_PROX2_PIN, R_PROX3_PIN, R_PROX4_PIN, R_PROX5_PIN};
+int weight_array[5]    = {-90,         -45,         0,           45,          90         };
 
-ProxSense frontProx(proximity_sensors_front, proximity_sensors_front_weight);
-
-void setup() 
+ProxSense frontProx(front_pin_array, weight_array);
+ProxSense rearProx (rear_pin_array,  weight_array);
+ 
+void setup()
 { 
   pinMode(SIGNAL_LED_PIN, OUTPUT);
-    digitalWrite(SIGNAL_LED_PIN, 0);
-    pinMode(BUZZER_PIN, OUTPUT);
-    analogWrite(BUZZER_PIN, 0);
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
-    pinMode(IMU_INTERRUPT_PIN, INPUT);
+  digitalWrite(SIGNAL_LED_PIN, 0);
+  pinMode(BUZZER_PIN, OUTPUT);
+  analogWrite(BUZZER_PIN, 0);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
 
-    ///////////////////Beginning Other PreSetup Scripts//////////////////////
+  ///////////////////Beginning Other PreSetup Scripts//////////////////////
 
-    //Set Serial Baud for debugging baud=bits per second
-    Serial.begin(BAUD);
-
-    // PWM resolution is 0-1023. wanted more than default
-    analogWriteResolution(PWM_SPEED_BITS);
-
-  
-  
   //Set Serial Baud for debugging baud=bits per second
   Serial.begin(BAUD);
-  
+
+  // PWM resolution is 0-1023. wanted more than default
+  analogWriteResolution(PWM_SPEED_BITS);
+
+  //Set Serial Baud for debugging baud=bits per second
+  Serial.begin(BAUD);
+
   //Check Battery
   if(analogRead(BATT_TEST_PIN) <= BATTERY_VOLTAGE_WARNING_COUNT)
   {
@@ -39,10 +38,14 @@ void setup()
 
 }
 void loop()
-{
-    int prox_front_error = frontProx.readAngle();
+{   
+  int front_angle = frontProx.readAngle();
+  int rear_angle = rearProx.readAngle();
 
-    Serial.println(prox_front_error);
-
-    delay(100);
+  Serial.print("Front Angle: \t");
+  Serial.println(front_angle);
+  Serial.print("\nRear Angle: \t");
+  Serial.println(rear_angle);
+  
+  delay(100);
 }
