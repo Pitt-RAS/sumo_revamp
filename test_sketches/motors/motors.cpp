1a4c5aeda03977af4cb3c36d1b0e95499df2e99c
asaba96 <asaba96@pitt.edu>
2017-04-06 20:20:34 -0400

    initial commit for motors test sketch

---
+++ test_sketches/motors/motors.cpp
@@ -0,0 +1,64 @@
+#include <Arduino.h>
+
+#include "motors.h"
+
+#include "../config.h"
+
+Motor::Motor(int motor_f_pin, int motor_f_pwm_pin, bool motor_f_forward_state){
+	pin_ = motor_f_pin;
+	pwm_pin_ = motor_f_pwm_pin;
+	forward_state_ = motor_f_forward_state;
+
+	//initializes pins for motors
+	pinMode(pin_, OUTPUT);
+	pinMode(pwm_pin_, OUTPUT);
+	
+}
+
+
+// input desired force and current speed
+static float idealMotorOutput(float force, float velocity) {
+    float required_current = force / FORCE_PER_AMP; 
+    float back_emf = velocity / VELOCITY_PER_VBEMF;
+    return ((required_current * RATED_INTERNAL_RESISTANCE + back_emf) / BATTERY_VOLTAGE);
+}
+
+
+void Motor::Set(float acceleration, float current_velocity) {
+    float force;
+    float speed;
+    int pin_state;
+    int speed_raw;
+
+    if (current_velocity > 0) {
+        force = (ROBOT_MASS * acceleration + FRICTION_FORCE) / NUMBER_OF_MOTORS;
+    } else if (current_velocity < 0) {
+        force = (ROBOT_MASS * acceleration - FRICTION_FORCE) / NUMBER_OF_MOTORS;
+    } else {
+        force = (ROBOT_MASS * acceleration) / NUMBER_OF_MOTORS;
+    }
+
+    speed = idealMotorOutput(force, current_velocity);
+    speed = constrain(speed, -1, 1);
+
+    speed_raw = abs((int)(round(PWM_SPEED_STEPS * speed)));
+
+    if (speed > 0.0) {
+        pin_state = HIGH;
+    } else {
+        pin_state = LOW;
+    }
+
+    digitalWrite(pin_, pin_state ^ forward_state_ ^ 1);
+    analogWrite(pwm_pin_, speed_raw);
+}
+
+void Motor::SetRaw(bool forward, int speed_raw) {
+    if (forward) {
+        digitalWrite(pin_, HIGH);
+    }
+    else {
+        digitalWrite(pin_, LOW);
+    }
+    analogWrite(pwm_pin_, speed_raw);
+}
---
+++ test_sketches/motors/motors.ino
@@ -0,0 +1,64 @@
+#include <Arduino.h>
+#include <EncoderPittMicromouse.h>
+#include "src/config.h"
+
+#include "src/state_machine/StateMachine.h"
+
+StateMachine stateMachine(CHARGE);
+
+const bool competition = false;
+
+void setup() { 
+
+    //MISC pins
+    pinMode(SIGNAL_LED_PIN, OUTPUT);
+    digitalWrite(SIGNAL_LED_PIN, 0);
+    pinMode(BUZZER_PIN, OUTPUT);
+    analogWrite(BUZZER_PIN, 0);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(IMU_INTERRUPT_PIN, INPUT);
+
+
+    ///////////////////Beginning Other PreSetup Scripts//////////////////////
+
+    //Set Serial Baud for debugging baud=bits per second
+    Serial.begin(BAUD);
+
+    // PWM resolution is 0-1023. wanted more than default
+    analogWriteResolution(PWM_SPEED_BITS);
+
+    // Set higher pwm frequency for smoother motor control.
+    analogWriteFrequency(L_MOTOR_PWM_PIN, 46875);
+    analogWriteFrequency(R_MOTOR_PWM_PIN, 46875);
+
+    //Check Battery
+    if(analogRead(BATT_TEST_PIN) <= BATTERY_VOLTAGE_WARNING_COUNT){
+        tone(BUZZER_PIN, 2000);
+    }
+
+
+    waitForButton();
+    stateMachine.deployRamps();
+}
+
+void loop(){
+    if (!competition && analogRead(BATT_TEST_PIN) <= BATTERY_VOLTAGE_WARNING_COUNT) {
+        tone(BUZZER_PIN, 2000);
+    }
+
+    stateMachine.updateState();
+    stateMachine.executeState();
+
+}
+
+void waitForButton(){
+    //Button Press
+    Serial.println("Waiting for Button");
+    while (digitalRead(BUTTON_PIN)){
+        delay(10); // possibly to prevent sumo.update from being called too often
+    }
+    Serial.println("Button Pressed");
+    delay(5000);
+}
+
+
